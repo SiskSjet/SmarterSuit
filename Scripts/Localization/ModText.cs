f6da8604861631792686aa081bea5f7a0db5fbaa
Sisk S'jet <sisk.sjet@gmail.com>
2018-09-22 10:14:01 +0200

    chore: implement files for localization support

---
+++ Scripts/Localization/ModText.cs
@@ -0,0 +1,14 @@
+﻿using VRage.Utils;
+
+// ReSharper disable InconsistentNaming
+namespace Sisk.SmarterSuit.Localization {
+    public class ModText {
+        public static readonly MyStringId Description_SS_Enable = MyStringId.Get(nameof(Description_SS_Enable));
+        public static readonly MyStringId Description_SS_Disable = MyStringId.Get(nameof(Description_SS_Disable));
+        public static readonly MyStringId Description_SS_List = MyStringId.Get(nameof(Description_SS_List));
+        public static readonly MyStringId Description_SS_Help = MyStringId.Get(nameof(Description_SS_Help));
+
+        public static readonly MyStringId Description_SS_NoPermission = MyStringId.Get(nameof(Description_SS_NoPermission));
+
+    }
+}
