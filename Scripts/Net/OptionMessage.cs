add23dbf0e533b0df196d8854dbc27b73f139749
Sisk S'jet <sisk.sjet@gmail.com>
2018-09-22 10:15:05 +0200

    chore: implement classes for settings and network data

---
+++ Scripts/Net/OptionMessage.cs
@@ -0,0 +1,21 @@
+﻿using ProtoBuf;
+using Sandbox.ModAPI;
+using Sisk.Utils.Net.Messages;
+
+// ReSharper disable ExplicitCallerInfoArgument
+namespace Sisk.SmarterSuit.Net {
+    [ProtoContract]
+    public class OptionMessage : IMessage {
+        [ProtoMember(1)]
+        public Option Option { get; set; }
+        [ProtoMember(2)]
+        public byte[] Value { get; set; }
+        public byte[] Serialize() {
+            return MyAPIGateway.Utilities.SerializeToBinary(this);
+        }
+    }
+
+    public enum Option {
+        AlwaysAutoHelmet
+    }
+}
---
+++ Scripts/Settings/ModSettings.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using System.Xml.Serialization;
+using ProtoBuf;
+
+// ReSharper disable ExplicitCallerInfoArgument
+
+namespace Sisk.SmarterSuit.Settings {
+    [ProtoContract]
+    public class ModSettings {
+        public const int VERSION = 1;
+        public const bool AUTO_HELMET_EVERYWHERE = false;
+
+        [ProtoMember(1)]
+        [XmlElement(Order = 1)]
+        public int Version { get; set; } = VERSION;
+
+        [ProtoMember(2)]
+        [DefaultValue(false)]
+        [XmlElement(Order = 2)]
+        public bool AlwaysAutoHelmet { get; set; } = AUTO_HELMET_EVERYWHERE;
+    }
+}
