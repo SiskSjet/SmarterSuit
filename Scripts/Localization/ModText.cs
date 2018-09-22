3169629b64f0856222ab6c9c4dca4eee59f3ffc8
Sisk S'jet <sisk.sjet@gmail.com>
2018-09-22 20:56:15 +0200

    style: format file to match style

--- Scripts/Localization/ModText.cs
+++ Scripts/Localization/ModText.cs
@@ -3,16 +3,16 @@
 // ReSharper disable InconsistentNaming
 namespace Sisk.SmarterSuit.Localization {
     public class ModText {
-        public static readonly MyStringId Description_SS_Disable = MyStringId.Get(nameof(Description_SS_Disable));
-        public static readonly MyStringId Description_SS_Enable = MyStringId.Get(nameof(Description_SS_Enable));
-        public static readonly MyStringId Description_SS_Help = MyStringId.Get(nameof(Description_SS_Help));
-        public static readonly MyStringId Description_SS_List = MyStringId.Get(nameof(Description_SS_List));
-        public static readonly MyStringId Description_SS_Set = MyStringId.Get(nameof(Description_SS_Set));
-        public static readonly MyStringId SS_ArgumentError = MyStringId.Get(nameof(SS_ArgumentError));
-        public static readonly MyStringId SS_ConvertError = MyStringId.Get(nameof(SS_ConvertError));
+        public static readonly MyStringId Description_SS_Disable = MyStringId.GetOrCompute(nameof(Description_SS_Disable));
+        public static readonly MyStringId Description_SS_Enable = MyStringId.GetOrCompute(nameof(Description_SS_Enable));
+        public static readonly MyStringId Description_SS_Help = MyStringId.GetOrCompute(nameof(Description_SS_Help));
+        public static readonly MyStringId Description_SS_List = MyStringId.GetOrCompute(nameof(Description_SS_List));
+        public static readonly MyStringId Description_SS_Set = MyStringId.GetOrCompute(nameof(Description_SS_Set));
+        public static readonly MyStringId SS_ArgumentError = MyStringId.GetOrCompute(nameof(SS_ArgumentError));
+        public static readonly MyStringId SS_ConvertError = MyStringId.GetOrCompute(nameof(SS_ConvertError));
 
-        public static readonly MyStringId SS_NoPermissionError = MyStringId.Get(nameof(SS_NoPermissionError));
-        public static readonly MyStringId SS_OnlyBooleanAllowedError = MyStringId.Get(nameof(SS_OnlyBooleanAllowedError));
-        public static readonly MyStringId SS_UnknownOptionError = MyStringId.Get(nameof(SS_UnknownOptionError));
+        public static readonly MyStringId SS_NoPermissionError = MyStringId.GetOrCompute(nameof(SS_NoPermissionError));
+        public static readonly MyStringId SS_OnlyBooleanAllowedError = MyStringId.GetOrCompute(nameof(SS_OnlyBooleanAllowedError));
+        public static readonly MyStringId SS_UnknownOptionError = MyStringId.GetOrCompute(nameof(SS_UnknownOptionError));
     }
 }
