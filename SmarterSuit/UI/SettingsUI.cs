386d0ee2f3aef0b8b33d2293a15920f7ee0eb107
Sisk S'jet <sisk.sjet@gmail.com>
2025-10-06 22:50:05 +0200

    ♻️: Refactors settings UI
    - Refactors the settings UI to improve organization and readability.
    - Combines fuel and dampener settings into a single "Jetpack" category.
    - Removes the "Align to gravity" setting from the main settings UI.

--- SmarterSuit/UI/SettingsUI.cs
+++ SmarterSuit/UI/SettingsUI.cs
@@ -19,45 +19,6 @@ namespace Sisk.SmarterSuit.UI {
 
         private void ClientReset() { }
 
-        private ControlCategory GetAlignToGravitySettings() {
-            var gravity = new ControlTile() {
-                new TerminalCheckbox() {
-                    Name = "Align to gravity",
-                    Value = Mod.Static.Settings.AlignToGravity,
-                    CustomValueGetter = () => Mod.Static.Settings.AlignToGravity,
-                    ControlChangedHandler = (sender, args) => Mod.Static.SetOption(Option.AlignToGravity, (sender as TerminalCheckbox).Value, false),
-                    ToolTip = new RichText(ToolTip.DefaultText) {
-                        "Will align the player to the gravity."
-                    },
-                },
-
-                new TerminalSlider() {
-                    Name = "Delay",
-                    CustomValueGetter = () => (float)Math.Round(Mod.Static.Settings.AlignToGravityDelay * 16f / 1000f, 0),
-                    ControlChangedHandler = (sender, args) => {
-                        var slider = sender as TerminalSlider;
-                        var value = Math.Round(slider.Value, 0);
-                        Mod.Static.SetOption(Option.AlignToGravityDelay, (int)Math.Round(value * 1000f / 16f, 0), false);
-                        slider.ValueText = $"{value} s";
-                    },
-                    ToolTip = new RichText(ToolTip.DefaultText) {
-                        "This will delay the align to gravity check for x ticks after the player toggled his helmet manualy."
-                    },
-                    Min = 0,
-                    Max = 30,
-                    ValueText = $"{Math.Round(Mod.Static.Settings.AlignToGravityDelay * 16f / 1000f, 0)} s",
-                }
-            };
-
-            return new ControlCategory() {
-                HeaderText = "Align to gravity",
-                SubheaderText = "Configure align to gravity behavior",
-                TileContainer = {
-                    gravity
-                },
-            };
-        }
-
         private ControlCategory GetBroadcastSettings() {
             var broadcast = new ControlTile() {
                 new TerminalCheckbox() {
@@ -76,95 +37,6 @@ namespace Sisk.SmarterSuit.UI {
                 SubheaderText = "Configure broadcast behavior",
                 TileContainer = {
                     broadcast
-                },
-            };
-        }
-
-        private ControlCategory GetDampenderSettings() {
-            var dropdown = new TerminalDropdown<DisableAutoDampenerOption>() {
-                Name = "Disable auto dampener",
-                ControlChangedHandler = (sender, args) => {
-                    var element = sender as TerminalDropdown<DisableAutoDampenerOption>;
-                    Mod.Static.SetOption(Option.DisableAutoDampener, element.Value.AssocObject, false);
-                },
-                ToolTip = new RichText(ToolTip.DefaultText) {
-                    "Will disable the auto dampener."
-                },
-            };
-
-            foreach (var preset in Enum.GetValues(typeof(DisableAutoDampenerOption)).Cast<DisableAutoDampenerOption>()) {
-                dropdown.List.Add(preset.ToString(), preset);
-            }
-
-            dropdown.Value = dropdown.List.FirstOrDefault(x => x.AssocObject == Mod.Static.Settings.DisableAutoDampener);
-            dropdown.CustomValueGetter = () => dropdown.List.FirstOrDefault(x => x.AssocObject == Mod.Static.Settings.DisableAutoDampener);
-
-            var dampener = new ControlTile() {
-                dropdown,
-                new TerminalSlider() {
-                    Name = "Halted speed tolerance",
-                    Value = Mod.Static.Settings.HaltedSpeedTolerance,
-                    CustomValueGetter = () => Mod.Static.Settings.HaltedSpeedTolerance,
-                    ControlChangedHandler = (sender, args) => {
-                        var slider = sender as TerminalSlider;
-                        var value = (float)Math.Round(slider.Value, 2);
-                        Mod.Static.SetOption(Option.HaltedSpeedTolerance, value, false);
-                        slider.ValueText = $"{value} %";
-                    },
-                    ToolTip = new RichText(ToolTip.DefaultText) {
-                        "The tolerance for the auto dampener."
-                    },
-                    Min = 0,
-                    Max = 2,
-                    ValueText = $"{Mod.Static.Settings.HaltedSpeedTolerance} m/s",
-                }
-            };
-
-            return new ControlCategory() {
-                HeaderText = "Dampener",
-                SubheaderText = "Configure dampener behavior",
-                TileContainer = {
-                    dampener
-                },
-            };
-        }
-
-        private ControlCategory GetFuelSettings() {
-            var fuel = new ControlTile() {
-                new TerminalCheckbox() {
-                    Name = "Additional fuel warning",
-                    Value = Mod.Static.Settings.AdditionalFuelWarning,
-                    CustomValueGetter = () => Mod.Static.Settings.AdditionalFuelWarning,
-                    ControlChangedHandler = (sender, args) => Mod.Static.SetOption(Option.AdditionalFuelWarning, (sender as TerminalCheckbox).Value, false),
-                    ToolTip = new RichText(ToolTip.DefaultText) {
-                        "Will show a warning if the fuel is below the threshold."
-                    },
-                },
-
-                new TerminalSlider() {
-                    Name = "Fuel threshold",
-                    Value = Mod.Static.Settings.FuelThreshold,
-                    CustomValueGetter = () => Mod.Static.Settings.FuelThreshold,
-                    ControlChangedHandler = (sender, args) => {
-                        var slider = sender as TerminalSlider;
-                        var value = (float)Math.Round(slider.Value, 2);
-                        Mod.Static.SetOption(Option.FuelThreshold, value, false);
-                        slider.ValueText = $"{value} %";
-                    },
-                    ToolTip = new RichText(ToolTip.DefaultText) {
-                        "The threshold for the fuel warning."
-                    },
-                    Min = 0,
-                    Max = 1,
-                    ValueText = $"{Mod.Static.Settings.FuelThreshold} %",
-                }
-            };
-
-            return new ControlCategory() {
-                HeaderText = "Fuel",
-                SubheaderText = "Configure fuel warning behavior",
-                TileContainer = {
-                    fuel
                 },
             };
         }
@@ -230,6 +102,116 @@ namespace Sisk.SmarterSuit.UI {
             };
         }
 
+        private ControlCategory GetJetpackSettings() {
+            var fuel = new ControlTile() {
+                new TerminalCheckbox() {
+                    Name = "Additional fuel warning",
+                    Value = Mod.Static.Settings.AdditionalFuelWarning,
+                    CustomValueGetter = () => Mod.Static.Settings.AdditionalFuelWarning,
+                    ControlChangedHandler = (sender, args) => Mod.Static.SetOption(Option.AdditionalFuelWarning, (sender as TerminalCheckbox).Value, false),
+                    ToolTip = new RichText(ToolTip.DefaultText) {
+                        "Will show a warning if the fuel is below the threshold."
+                    },
+                },
+
+                new TerminalSlider() {
+                    Name = "Fuel threshold",
+                    Value = Mod.Static.Settings.FuelThreshold,
+                    CustomValueGetter = () => Mod.Static.Settings.FuelThreshold,
+                    ControlChangedHandler = (sender, args) => {
+                        var slider = sender as TerminalSlider;
+                        var value = (float)Math.Round(slider.Value, 2);
+                        Mod.Static.SetOption(Option.FuelThreshold, value, false);
+                        slider.ValueText = $"{value} %";
+                    },
+                    ToolTip = new RichText(ToolTip.DefaultText) {
+                        "The threshold for the fuel warning."
+                    },
+                    Min = 0,
+                    Max = 1,
+                    ValueText = $"{Mod.Static.Settings.FuelThreshold} %",
+                }
+            };
+
+            var dropdown = new TerminalDropdown<DisableAutoDampenerOption>() {
+                Name = "Disable auto dampener",
+                ControlChangedHandler = (sender, args) => {
+                    var element = sender as TerminalDropdown<DisableAutoDampenerOption>;
+                    Mod.Static.SetOption(Option.DisableAutoDampener, element.Value.AssocObject, false);
+                },
+                ToolTip = new RichText(ToolTip.DefaultText) {
+                    "Will disable the auto dampener."
+                },
+            };
+
+            foreach (var preset in Enum.GetValues(typeof(DisableAutoDampenerOption)).Cast<DisableAutoDampenerOption>()) {
+                dropdown.List.Add(preset.ToString(), preset);
+            }
+
+            dropdown.Value = dropdown.List.FirstOrDefault(x => x.AssocObject == Mod.Static.Settings.DisableAutoDampener);
+            dropdown.CustomValueGetter = () => dropdown.List.FirstOrDefault(x => x.AssocObject == Mod.Static.Settings.DisableAutoDampener);
+
+            var dampener = new ControlTile() {
+                dropdown,
+                new TerminalSlider() {
+                    Name = "Halted speed tolerance",
+                    Value = Mod.Static.Settings.HaltedSpeedTolerance,
+                    CustomValueGetter = () => Mod.Static.Settings.HaltedSpeedTolerance,
+                    ControlChangedHandler = (sender, args) => {
+                        var slider = sender as TerminalSlider;
+                        var value = (float)Math.Round(slider.Value, 2);
+                        Mod.Static.SetOption(Option.HaltedSpeedTolerance, value, false);
+                        slider.ValueText = $"{value} %";
+                    },
+                    ToolTip = new RichText(ToolTip.DefaultText) {
+                        "The tolerance for the auto dampener."
+                    },
+                    Min = 0,
+                    Max = 2,
+                    ValueText = $"{Mod.Static.Settings.HaltedSpeedTolerance} m/s",
+                }
+            };
+
+            var gravity = new ControlTile() {
+                new TerminalCheckbox() {
+                    Name = "Align to gravity",
+                    Value = Mod.Static.Settings.AlignToGravity,
+                    CustomValueGetter = () => Mod.Static.Settings.AlignToGravity,
+                    ControlChangedHandler = (sender, args) => Mod.Static.SetOption(Option.AlignToGravity, (sender as TerminalCheckbox).Value, false),
+                    ToolTip = new RichText(ToolTip.DefaultText) {
+                        "Will align the player to the gravity."
+                    },
+                },
+
+                new TerminalSlider() {
+                    Name = "Delay",
+                    CustomValueGetter = () => (float)Math.Round(Mod.Static.Settings.AlignToGravityDelay * 16f / 1000f, 0),
+                    ControlChangedHandler = (sender, args) => {
+                        var slider = sender as TerminalSlider;
+                        var value = Math.Round(slider.Value, 0);
+                        Mod.Static.SetOption(Option.AlignToGravityDelay, (int)Math.Round(value * 1000f / 16f, 0), false);
+                        slider.ValueText = $"{value} s";
+                    },
+                    ToolTip = new RichText(ToolTip.DefaultText) {
+                        "This will delay the align to gravity check for x ticks after the player toggled his helmet manualy."
+                    },
+                    Min = 0,
+                    Max = 30,
+                    ValueText = $"{Math.Round(Mod.Static.Settings.AlignToGravityDelay * 16f / 1000f, 0)} s",
+                }
+            };
+
+            return new ControlCategory() {
+                HeaderText = "Jetpack",
+                SubheaderText = "Configure jetpack settings",
+                TileContainer = {
+                    fuel,
+                    dampener,
+                    gravity
+                },
+            };
+        }
+
         private void HudInit() {
             RichHudTerminal.Root.Enabled = true;
 
@@ -238,9 +220,7 @@ namespace Sisk.SmarterSuit.UI {
                     Name = "Settings",
                     CategoryContainer = {
                         GetHelmetSettings(),
-                        GetFuelSettings(),
-                        GetDampenderSettings(),
-                        GetAlignToGravitySettings(),
+                        GetJetpackSettings(),
                         GetBroadcastSettings()
                     },
                 },
